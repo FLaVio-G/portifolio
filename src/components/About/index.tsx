369c81b8eaaf275468d9de6f688a852658c64e19
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-03-20 02:54:33 -0300

    Alterações no about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,5 +1,12 @@
-import { AreaInfo, Container, ImageArea, ImageF, ImageUser, InfoDetail, MoreInfo, SubDetail, Title, Wrapper } from "./styles";
-import User from '../../assets/flavio.jpg';
+import { AreaInfo, BarDetail, Container, ImageArea, ImageF, ImageUser, InfoDetail, MoreInfo, SubDetail, Title, Wrapper } from "./styles";
+
+import { BsGithub } from 'react-icons/bs';
+import { FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMailOpen } from 'react-icons/hi';
+
+
+
+const User = require("../../assets/flavio.jpg")
 
 function About() {
   return (
@@ -8,28 +15,31 @@ function About() {
 
       <Wrapper>
         <ImageArea>
-   <ImageF>
-            <ImageUser src={User}/>
-            </ImageF>
-            <SubDetail>linkedln: https://www.linkedin.com/in/fl%C3%A1vioviana113/ </SubDetail>
-          <SubDetail>gitHub: https://github.com/KaTSu113</SubDetail>
+          <ImageF>
+            <ImageUser src={User} />
+          </ImageF>
+          <BarDetail>
+          <SubDetail> <BsGithub /> </SubDetail>
+          <SubDetail> <FaLinkedin /> </SubDetail>
+          <SubDetail> < HiOutlineMailOpen /> </SubDetail>
+          </BarDetail>
         </ImageArea>
       </Wrapper>
 
       <AreaInfo>
-        <Title> 
+        <Title>
           Sobre
         </Title>
         <MoreInfo>
-        Olá meu nome é Flávio, sou apaixonado por tecnologia. Meu primeiro contato com esse mundo foi quando 
-        meu Avô comprou um computador, pórem não tinha sistema operacional instalado, a partir daí começou essa saga, sendo o primeiro
-       checkpoint "Instalar o windows xp!
+          Olá meu nome é Flávio, sou apaixonado por tecnologia. Meu primeiro contato com esse mundo foi quando
+          meu Avô comprou um computador, pórem não tinha sistema operacional instalado, a partir daí começou essa saga, sendo o primeiro
+          checkpoint "Instalar o windows xp!
         </MoreInfo>
         <InfoDetail>
           <SubDetail>Flavio Gabriel de Andrade Viana </SubDetail>
           <SubDetail>Idade: 23 anos </SubDetail>
           <SubDetail>Contato: (93)981218330 / (93)988019961 </SubDetail>
-          
+
 
         </InfoDetail>
 
